86db157e98ce10ead2e5a2e3c7bf37e85b9fba26
Jeevitha <jeevithak@stgit.com>
2026-02-04 14:50:21 +0530

    first commit

---
+++ src/main/java/com/stg/azureDemo/controller/AzureController.java
@@ -0,0 +1,23 @@
+package com.stg.azureDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/test")
+public class AzureController {
+
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("message", "The backend is running !");
+        return response;
+    }
+
+
+}
